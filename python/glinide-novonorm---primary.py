d3bf37f5489da15e5c15a6c998166e490cdbea5e
Martin Chapman <contact@martinchapman.co.uk>
2024-05-27 11:29:45 +0100

    Update made by Phenoflow

---
+++ python/glinide-novonorm---primary.py
@@ -0,0 +1,25 @@
+# Victor W Zhong, Juhaeri Juhaeri, Stephen R Cole, Christian M Shay, Carolyn A Chew-Graham, Penny Gordon-Larsen, Evangelos Kontopantelis, Elizabeth J Mayer-Davis, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"11321","system":"gprdproduct"},{"code":"11366","system":"gprdproduct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('glinide-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["glinide-novonorm---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["glinide-novonorm---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["glinide-novonorm---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
